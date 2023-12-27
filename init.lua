07f43b07c2565df89fa59dc0ba99478f80e6ad66
Quinten Konyn <qkonyn@gmail.com>
2023-12-26 23:50:33 -0800

    Enable rust lsp

--- init.lua
+++ init.lua
@@ -567,7 +567,7 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
